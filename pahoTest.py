3e2c5ec7e3abc3cde22d2d5026653901ee733354
VincentGaudreault <71224359+VincentGaudreault@users.noreply.github.com>
2021-03-26 09:56:31 -0400

    Update pahoTest.py

--- pahoTest.py
+++ pahoTest.py
@@ -1,8 +1,19 @@
 import paho.mqtt.client as mqtt
+import time
+
+connected = False
+messageOk = False
+
 
 broker = "broker.mqttdashboard.com"
+port = 1883
 
 client = mqtt.Client("python1")
-print("yooooo", client)
-client.connect(broker)
 
+print("yooooo", client)
+
+client.connect(broker, port=port)
+client.loop_start()
+client.publish("testlashit", "woot")
+client.loop_stop()
+client.disconnect()
