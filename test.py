c20ce088dc28a19d09b5e7c3c105f581b423d71a
VincentGaudreault <71224359+VincentGaudreault@users.noreply.github.com>
2021-03-27 13:55:27 -0400

    Update test.py

--- test.py
+++ test.py
@@ -3,8 +3,5 @@ from ConnectionMqtt import *
 
 camera = CameraDAO()
 camera.prendrePhoto()
-# print(conversionImage("NewPicture.jpg"))
 controleur = ConnectionMqtt("henry", "broker.mqttdashboard.com", 1883)
-# print(controleur)
-#controleur.publish("test/mqtt", conversionImage("NewPicture.jpg"))
 controleur.envoyerImage("test/mqtt", "NewPicture.jpg")
