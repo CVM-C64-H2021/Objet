bb07a7655becdb733e609170685c655e61f23237
VincentGaudreault <71224359+VincentGaudreault@users.noreply.github.com>
2021-03-27 13:58:12 -0400

    Update ConnectionMqtt.py

--- ConnectionMqtt.py
+++ ConnectionMqtt.py
@@ -10,8 +10,9 @@ class ConnectionMqtt():
         self.port = port
         self.nom = nomUtilisateur
 
-    def publish(self, topic, message):
-        publish.single(topic, message, hostname=self.broker)
+    def closeConnect(self):
+        self.client.disconnect()
+        self.client.loop_stop()
 
     def connect_mqtt(self) -> mqtt:
         def on_connect(client, userdata, flags, rc):
@@ -24,6 +25,14 @@ class ConnectionMqtt():
         self.client.on_connect = on_connect
         self.client.connect(self.broker, self.port)
 
+    def envoyerImage(self, topic, image):
+        with open(image, "rb") as imgFile:
+            myString = base64.b64encode(imgFile.read())
+        self.publish(topic, myString)
+
+    def publish(self, topic, message):
+        publish.single(topic, message, hostname=self.broker)
+
     def subscribe(self, topic):
         self.connect_mqtt()
 
@@ -34,12 +43,3 @@ class ConnectionMqtt():
         self.client.subscribe(topic)
         self.client.on_message = on_message
         self.client.loop_forever()
-
-    def closeConnect(self):
-        self.client.disconnect()
-        self.client.loop_stop()
-
-    def envoyerImage(self, topic, image):
-        with open(image, "rb") as imgFile:
-            myString = base64.b64encode(imgFile.read())
-        self.publish(topic, myString)
