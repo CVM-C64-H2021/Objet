c86c0ff74eada6b1e4a416dfb9a388bf7dad78f8
VincentGaudreault <71224359+VincentGaudreault@users.noreply.github.com>
2021-04-30 08:35:48 -0400

    objet

--- face_rec.py
+++ face_rec.py
@@ -4,50 +4,49 @@ import numpy as np
 from time import sleep
 
 
-def get_encoded_faces():
-    """
-    looks through the faces folder and encodes all
-    the faces
+class FaceRec:
 
-    :return: dict of (name, image encoded)
-    """
-    encoded = {}
+    def get_encoded_faces(self):
+        """
+        looks through the faces folder and encodes all
+        the faces
 
-    for dirpath, dnames, fnames in os.walk("./Faces"):
-        for f in fnames:
-            if f.endswith(".jpg") or f.endswith(".png"):
-                face = fr.load_image_file("Faces/" + f)
-                encoding = fr.face_encodings(face)[0]
-                encoded[f.split(".")[0]] = encoding
+        :return: dict of (name, image encoded)
+        """
+        encoded = {}
 
-    return encoded
+        for dirpath, dnames, fnames in os.walk("./Faces"):
+            for f in fnames:
+                if f.endswith(".jpg") or f.endswith(".png"):
+                    face = fr.load_image_file("Faces/" + f)
+                    encoding = fr.face_encodings(face)[0]
+                    encoded[f.split(".")[0]] = encoding
 
+        return encoded
 
-def classify_face(img):
-    """
-    will find all of the faces in a given image and label
-    them if it knows what they are
+    def classify_face(self, img):
+        """
+        will find all of the faces in a given image and label
+        them if it knows what they are
 
-    :param im: str of file path
-    :return: list of face names
-    """
-    faces = get_encoded_faces()
-    faces_encoded = list(faces.values())
-    known_face_names = list(faces.keys())
- 
-    face_locations = fr.face_locations(img)
-    unknown_face_encodings = fr.face_encodings(img, face_locations)
+        :param im: str of file path
+        :return: list of face names
+        """
+        faces = self.get_encoded_faces()
+        faces_encoded = list(faces.values())
+        known_face_names = list(faces.keys())
 
-    face_names = []
-    numberOfMatches = 0
-    for face_encoding in unknown_face_encodings:
-        # See if the face is a match for the known face(s)
-        matches = fr.compare_faces(faces_encoded, face_encoding)
- 
-        for i in matches:
-            if i  == True:
-                numberOfMatches += 1
+        face_locations = fr.face_locations(img)
+        unknown_face_encodings = fr.face_encodings(img, face_locations)
 
-       
+        face_names = []
+        numberOfMatches = 0
+        for face_encoding in unknown_face_encodings:
+            # See if the face is a match for the known face(s)
+            matches = fr.compare_faces(faces_encoded, face_encoding)
 
-    return numberOfMatches
+            for i in matches:
+                if i == True:
+                    numberOfMatches += 1
+
+        return numberOfMatches
